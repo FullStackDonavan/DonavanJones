aef281229ef6d238b847dc4b61f0db33b9628bda
DonavanJones <donavancjones@gmail.com>
2024-08-13 16:08:30 -0400

    first commit

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -18,20 +18,20 @@
             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">
               Resources
             </h2>
-            <ul class="text-gray-800 dark:text-gray-400">
+            <!-- <ul class="text-gray-800 dark:text-gray-400">
               <li class="mb-4">
                 <a href="/" class="hover:underline">Flowbite</a>
               </li>
               <li>
                 <a href="/" class="hover:underline">Tailwind CSS</a>
               </li>
-            </ul>
+            </ul> -->
           </div>
           <div>
             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">
-              Follow us
+              Follow me
             </h2>
-            <ul class="text-gray-800 dark:text-gray-400">
+            <!-- <ul class="text-gray-800 dark:text-gray-400">
               <li class="mb-4">
                 <a
                   href="https://github.com/themesberg/flowbite"
@@ -44,7 +44,7 @@
                   >Discord</a
                 >
               </li>
-            </ul>
+            </ul> -->
           </div>
           <div>
             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">
@@ -66,14 +66,13 @@
       />
       <div class="sm:flex sm:items-center sm:justify-between">
         <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"
-          >© 2023
-          <a href="https://flowbite.com" class="hover:underline"
-            >True Coat Marketing LLC</a
-          >. All Rights Reserved.
+          >© 2024
+          <NuxtLink to="/" class="hover:underline">Donavan Jones</NuxtLink>. All
+          Rights Reserved.
         </span>
         <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-          <a
-            href="#"
+          <NuxtLink
+            to="#"
             class="text-gray-800 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -88,7 +87,7 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </NuxtLink>
           <!-- <a
             href="#"
             class="text-gray-800 hover:text-gray-900 dark:hover:text-white"
@@ -106,8 +105,8 @@
               />
             </svg>
           </a> -->
-          <a
-            href="#"
+          <NuxtLink
+            to="#"
             class="text-gray-800 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -120,7 +119,7 @@
                 d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
               />
             </svg>
-          </a>
+          </NuxtLink>
           <!-- <a
             href="#"
             class="text-gray-800 hover:text-gray-900 dark:hover:text-white"
