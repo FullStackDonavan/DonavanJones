ac2830c90af427c7b20f94fdd82de3650e36a65f
DonavanJones <donavancjones@gmail.com>
2024-08-17 16:57:58 -0400

    first commit

---
+++ components/SkeletonLoaders/SkeletonLoader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="animate-pulse">
+    <!-- Skeleton for image -->
+    <div class="h-48 bg-gray-300 dark:bg-gray-700 rounded-t-lg mb-4"></div>
+    <!-- Skeleton for title -->
+    <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded mb-2"></div>
+    <!-- Skeleton for description -->
+    <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-3/4"></div>
+    <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-2/4 mt-2"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- pages/tags/[name].vue
+++ pages/tags/[name].vue
@@ -1,6 +1,85 @@
+<template>
+  <PatternSection>
+    <div class="container mx-auto px-4 py-8">
+      <nuxt-link
+        v-if="previousRoute"
+        :to="previousRoute"
+        class="block cursor-pointer mb-4 text-blue-500 hover:underline"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="inline h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11 17l-5-5m0 0l5-5m-5 5h12"
+          />
+        </svg>
+        Back
+      </nuxt-link>
+
+      <h1 class="text-2xl font-bold mb-4 uppercase">
+        {{ route.params.name }} Projects
+      </h1>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <!-- Skeleton Loader when content is loading -->
+        <template v-if="pending">
+          <SkeletonLoader v-for="i in limit" :key="i" />
+        </template>
+
+        <!-- Content when loaded -->
+        <template v-else>
+          <div
+            v-if="articles.length === 0"
+            class="col-span-full text-center text-gray-500"
+          >
+            No projects found for {{ route.params.name }}.
+          </div>
+          <div
+            v-for="(article, index) in articles"
+            :key="index"
+            class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 hover:scale-105 transition-transform duration-500"
+          >
+            <NuxtImg
+              v-if="article.excerptImage"
+              :src="article.excerptImage"
+              quality="80"
+              format="webp"
+              :alt="article.title"
+              class="w-full h-48 object-cover rounded-t-lg mb-4"
+            />
+            <h2 class="text-xl font-semibold mb-2">{{ article.title }}</h2>
+            <NuxtLink
+              :to="{ path: article._path, query: { from: route.fullPath } }"
+              class="text-blue-500 hover:underline mt-2 inline-block"
+            >
+              Read more
+            </NuxtLink>
+          </div>
+        </template>
+      </div>
+
+      <!-- Pagination -->
+      <Pagination
+        :currentPage="currentPage"
+        :limit="limit"
+        :totalPages="totalPages"
+        v-if="totalPages > 1"
+      />
+    </div>
+  </PatternSection>
+</template>
+
 <script setup lang="ts">
 import { ref, onMounted, watch, computed } from "vue";
 import { useRoute } from "#app";
+import SkeletonLoader from "~/components/SkeletonLoader.vue";
 
 const route = useRoute();
 const articles = ref([]);
@@ -27,17 +106,14 @@ const fetchArticles = async () => {
   }
 };
 
-// Fetch articles on component mount and when the page or tag name changes
 onMounted(fetchArticles);
 
 watch([() => route.params.name, currentPage], fetchArticles);
 
-// Truncate description function
 const truncateDescription = (text: string, length: number) => {
   return text.length > length ? text.substring(0, length) + "..." : text;
 };
 
-// Get the previous route to handle back navigation
 const previousRoute = ref<string | null>(null);
 
 onMounted(() => {
@@ -47,7 +123,6 @@ onMounted(() => {
   }
 });
 
-// Compute the total number of pages based on the total number of articles
 const { data: totalArticlesCount } = await useAsyncData(
   "totalArticlesCount",
   () =>
@@ -60,75 +135,3 @@ const totalPages = computed(() =>
   Math.ceil(totalArticlesCount.value / limit.value)
 );
 </script>
-<template>
-  <div class="container mx-auto px-4 py-8">
-    <nuxt-link
-      v-if="previousRoute"
-      :to="previousRoute"
-      class="block cursor-pointer mb-4 text-blue-500 hover:underline"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="inline h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M11 17l-5-5m0 0l5-5m-5 5h12"
-        />
-      </svg>
-      Back
-    </nuxt-link>
-
-    <h1 class="text-2xl font-bold mb-4 uppercase">
-      {{ route.params.name }} Projects
-    </h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <div v-if="pending" class="col-span-full text-center text-gray-500">
-        Loading...
-      </div>
-      <div
-        v-else-if="articles.length === 0"
-        class="col-span-full text-center text-gray-500"
-      >
-        No projects found for {{ route.params.name }}.
-      </div>
-      <div
-        v-for="(article, index) in articles"
-        :key="index"
-        class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 hover:scale-105 transition-transform duration-500"
-      >
-        <NuxtImg
-          v-if="article.excerptImage"
-          :src="article.excerptImage"
-          quality="80"
-          format="webp"
-          :alt="article.title"
-          class="w-full h-48 object-cover rounded-t-lg mb-4"
-        />
-        <h2 class="text-xl font-semibold mb-2">{{ article.title }}</h2>
-        <!-- <p class="text-gray-700 dark:text-gray-300">
-          {{ truncateDescription(article.description, 100) }}
-        </p> -->
-        <NuxtLink
-          :to="{ path: article._path, query: { from: route.fullPath } }"
-          class="text-blue-500 hover:underline mt-2 inline-block"
-        >
-          Read more
-        </NuxtLink>
-      </div>
-    </div>
-
-    <!-- Pagination -->
-    <Pagination
-      :currentPage="currentPage"
-      :limit="limit"
-      :totalPages="totalPages"
-      v-if="totalPages > 1"
-    />
-  </div>
-</template>
